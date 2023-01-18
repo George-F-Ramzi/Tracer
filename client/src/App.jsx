e4142eff4b253b2f76b364881c1e99e4f367fe24
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-01-18 15:56:11 +0200

    playing around

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,39 @@
 import React from "react";
+import { fabric } from "fabric";
 
 const App = () => {
-  return (
-    <div>
-      <canvas className="cav" />
-    </div>
-  );
+  const fabricRef = React.useRef(null);
+  const canvasRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const initFabric = () => {
+      fabricRef.current = new fabric.Canvas(canvasRef.current);
+    };
+
+    const addRectangle = () => {
+      const rect = new fabric.Circle({
+        top: 50,
+        left: 50,
+        radius: 20,
+        fill: "red",
+      });
+
+      fabricRef.current.add(rect);
+    };
+
+    const disposeFabric = () => {
+      fabricRef.current.dispose();
+    };
+
+    initFabric();
+    addRectangle();
+
+    return () => {
+      disposeFabric();
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} />;
 };
 
 export default App;
