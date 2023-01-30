6942077ee8c1755e59328dafac31e82944cb8a76
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-01-30 16:26:24 +0200

    Knowing The Problem

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,41 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("cav");
+
 const ctx = canvas.getContext("2d");
-const fileinput = document.getElementById("file--input");
 
-const srcImage = new Image();
-
-let imgData = null;
-let originalPixels = null;
-let currentPixels = null;
-let loaded = false;
-
-let cameraZoom = 1;
-const MAX_ZOOM = 10;
-const MIN_ZOOM = 0.2;
-const zoomSensitivity = 0.002;
-
-let centerX = window.innerWidth / 2;
-let centerY = window.innerHeight / 2;
-
-let centerXOBJ;
-let centerYOBJ;
-
-fileinput.onchange = function (e) {
-  loaded = false;
-  if (e.target.files && e.target.files.item(0)) {
-    srcImage.src = URL.createObjectURL(e.target.files[0]);
-  }
-};
-
-srcImage.onload = function () {
-  centerXOBJ = 0 - srcImage.width / 2;
-  centerYOBJ = 0 - srcImage.height / 2;
-  loaded = true;
-};
+let offsetX = window.innerWidth / 2;
+let offsetY = window.innerHeight / 2;
 
 draw();
 
 function draw() {
-  canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
 
-  ctx.translate(centerX, centerY);
-  ctx.scale(cameraZoom, cameraZoom);
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  ctx.translate(offsetX, offsetY);
 
-  if (loaded) {
-    ctx.drawImage(
-      srcImage,
-      centerXOBJ,
-      centerYOBJ,
-      srcImage.width,
-      srcImage.height
-    );
-    imgData = ctx.getImageData(0, 0, srcImage.width, srcImage.height);
-    originalPixels = imgData.data.slice();
-  }
-
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 100, 100);
   requestAnimationFrame(draw);
 }
 
-canvas.addEventListener("wheel", (e) => {
-  cameraZoom -= e.deltaY * zoomSensitivity;
-  cameraZoom = Math.min(cameraZoom, MAX_ZOOM);
-  cameraZoom = Math.max(cameraZoom, MIN_ZOOM);
+let canDrag = false;
+let startX = 0;
+let startY = 0;
+
+canvas.addEventListener("pointerdown", (e) => {
+  canDrag = true;
+  startX = e.clientX - offsetX;
+  startY = e.clientY - offsetY;
+});
+
+canvas.addEventListener("pointermove", (e) => {
+  if (canDrag) {
+    offsetX = e.clientX - startX;
+    offsetY = e.clientY - startY;
+  }
+});
+
+canvas.addEventListener("pointerup", () => {
+  canDrag = false;
 });
