3d9aba1a473844500d4d3838e7718ef504cb872e
George-F-Ramzi <109841377+George-F-Ramzi@users.noreply.github.com>
2023-01-31 12:49:22 +0200

    Understanding The Image Data

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,9 @@ const canvas = document.getElementById("cav");
 
 const ctx = canvas.getContext("2d");
 
-let offsetX = window.innerWidth / 2;
-let offsetY = window.innerHeight / 2;
+let ctxPosX = window.innerWidth / 2;
+let ctxPosY = window.innerHeight / 2;
+let scale = 1;
 
 draw();
 
@@ -11,9 +12,14 @@ function draw() {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
 
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  ctx.translate(offsetX, offsetY);
+  ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
+  ctx.scale(scale, scale);
+  ctx.translate(
+    -window.innerWidth / 2 + ctxPosX,
+    -window.innerHeight / 2 + ctxPosY
+  );
 
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   ctx.fillStyle = "red";
   ctx.fillRect(0, 0, 100, 100);
   requestAnimationFrame(draw);
@@ -25,17 +31,23 @@ let startY = 0;
 
 canvas.addEventListener("pointerdown", (e) => {
   canDrag = true;
-  startX = e.clientX - offsetX;
-  startY = e.clientY - offsetY;
+  startX = e.clientX - ctxPosX;
+  startY = e.clientY - ctxPosY;
 });
 
 canvas.addEventListener("pointermove", (e) => {
   if (canDrag) {
-    offsetX = e.clientX - startX;
-    offsetY = e.clientY - startY;
+    ctxPosX = e.clientX - startX;
+    ctxPosY = e.clientY - startY;
   }
 });
 
 canvas.addEventListener("pointerup", () => {
   canDrag = false;
+});
+
+canvas.addEventListener("wheel", (e) => {
+  scale -= e.deltaY * 0.0005;
+  scale = Math.min(scale, 15);
+  scale = Math.max(scale, 0.1);
 });
